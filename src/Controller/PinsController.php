933e13875388ae8a80e79bc3a4d2f31cd2aecba1
Kévin Zochowski <kevin.zochowski@gmail.com>
2023-04-18 15:51:47 +0200

    Ajout de la méthode create avec formBuilder et logique

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -6,11 +6,15 @@ use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PinsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home', methods:['GET'])]
     public function index(PinRepository $pinRepository): Response
     {
         $pins = $pinRepository->findBy([],['createdAt' => 'DESC']);
@@ -18,6 +22,30 @@ class PinsController extends AbstractController
     }
     #[Route('/pins/{id<[0-9]+>}', name:'app_pins_show')]
     public function show(Pin $pin): Response{
-        return$this->render('pins/show.html.twig', compact('pin'));
+        return $this->render('pins/show.html.twig', compact('pin'));
+    }
+    #[Route('/pins/create', name:'app_pins_create', methods:['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response{
+        $pin = new Pin;
+
+        $form = $this   ->createFormBuilder($pin)
+                        ->add('title', TextType::class)
+                        ->add('description', TextareaType::class)
+                        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // $data = $form->getData();
+            // $pin = new Pin;
+            // $pin->setTitle($data['title']);
+            // $pin->setDescription($data['description']);
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig', ['formCreate'=>$form->createView()]);
     }
 }
