4d55d04bd31c35a6047f89d476a5e605c8aef6fd
Kévin Zochowski <kevin.zochowski@gmail.com>
2023-04-18 10:26:33 +0200

    Création d'un repository Pin et d'une fonction exploitable en twig

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PinRepository;
 
 class PinsController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PinRepository $pinRepository): Response
     {
-        return $this->render('pins/index.html.twig');
+        $pins = $pinRepository->findAll();
+        return $this->render('pins/index.html.twig', compact('pins'));
     }
 }
