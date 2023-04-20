8b25e2ceafa16f22af0115d8b661eb76b8aade1f
Kévin Zochowski <kevin.zochowski@gmail.com>
2023-04-20 08:41:31 +0200

    Ajout de la méthode edit avec formBuilder et logique

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 
+
 class PinsController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods:['GET'])]
@@ -20,10 +21,12 @@ class PinsController extends AbstractController
         $pins = $pinRepository->findBy([],['createdAt' => 'DESC']);
         return $this->render('pins/index.html.twig', compact('pins'));
     }
+
     #[Route('/pins/{id<[0-9]+>}', name:'app_pins_show')]
     public function show(Pin $pin): Response{
         return $this->render('pins/show.html.twig', compact('pin'));
     }
+
     #[Route('/pins/create', name:'app_pins_create', methods:['GET', 'POST'])]
     public function create(Request $request, EntityManagerInterface $em): Response{
         $pin = new Pin;
@@ -48,4 +51,24 @@ class PinsController extends AbstractController
 
         return $this->render('pins/create.html.twig', ['formCreate'=>$form->createView()]);
     }
+
+    #[Route('/pins/{id<[0-9]+>}/edit', name:'app_pins_edit', methods:['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $em, Pin $pin): Response{
+
+        $form = $this   ->createFormBuilder($pin)
+                        ->add('title', TextType::class)
+                        ->add('description', TextareaType::class)
+                        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/edit.html.twig', ['pin'=>$pin, 'form'=>$form->createView()]);
+    }
+
 }
