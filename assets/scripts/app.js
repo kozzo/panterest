0b06b30cf27fa3bf334f16e13f0e05f43e1cd7e5
Kévin Zochowski <kevin.zochowski@gmail.com>
2023-04-21 10:54:43 +0200

    Installation du webpack Encore et emploi du SCSS

---
+++ assets/scripts/app.js
@@ -0,0 +1,16 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../styles/app.scss';
+
+// start the Stimulus application
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import '../../node_modules/@popperjs/core/dist/umd/popper.min.js';
